aad431c161e39d47711bde81435b91ce16f2fefd
Adrián <47431198+fmadrian@users.noreply.github.com>
2024-10-18 10:41:21 -0600

    Utteranc component added

---
+++ components/global/utteranc.vue
@@ -0,0 +1,24 @@
+<template>
+</template>
+<script>
+
+export default {
+    layout: "default",
+
+    head: {
+        script: [
+            {
+                src: 'https://utteranc.es/client.js',
+                repo: "fmadrian/eif511-tarea3",
+                'issue-term': "pathname",
+                label: "comments",
+                theme: "github-light",
+                crossorigin: "anonymous",
+                body: true,
+                async: true
+            }
+        ]
+    },
+
+};
+</script>
